5b51930025e4b07d9d5a5a11f0adffb45599b371
Nikita Goncharuk <retretret.r@mail.ru>
2018-07-11 10:49:59 +0300

    Add project files.

---
+++ src/AndroidColorsTest.Core/App.cs
@@ -0,0 +1,19 @@
+﻿using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+using AndroidColorsTest.Core.ViewModels.Home;
+
+namespace AndroidColorsTest.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<HomeViewModel>();
+        }
+    }
+}
---
+++ src/AndroidColorsTest.Core/ViewModels/!Base/BaseViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AndroidColorsTest.Core.ViewModels
+{
+    public abstract class BaseViewModel : MvxViewModel
+    {
+    }
+}
